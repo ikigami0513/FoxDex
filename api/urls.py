4b73731334dbfa7b9e645134657ca1b8b893b375
ikigami0513 <ikigami0513@gmail.com>
2023-07-21 23:54:10 +0200

    ajout de la vue pokemon dans l'api

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from rest_framework.authtoken import views as rest_views
 from .views import *
 
 urlpatterns = [
-    path('get_token', rest_views.obtain_auth_token, name='token'),
+    path('token/', rest_views.obtain_auth_token, name='token'),
+    path('pokemon/<int:numero>/', PokemonNumeroApiView.as_view(), name='api_pokemon_numero'),
+    path('pokemon/<str:name>/', PokemonNameApiView.as_view(), name='api_pokemon_name'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import View
+from app.models import *
 
-# Create your views here.
+class PokemonNumeroApiView(View):
+    def get(self, request, numero):
+        try:
+            pokemon = Pokemon.objects.get(numero=numero)
+        except Pokemon.DoesNotExist:
+            return render(request, 'error404.json', {'error': f'pokemon {numero} did not found'}, content_type='application/json')
+            
+        return render(request, 'pokemon.json', {'pokemon': pokemon}, content_type='application/json')
+    
+class PokemonNameApiView(View):
+    def get(self, request, name):
+        try:
+            pokemon = Pokemon.objects.get(name=name.capitalize())
+        except Pokemon.DoesNotExist:
+            return render(request, 'error404.json', {'error': f'pokemon {name} did not found'}, content_type='application/json')
+        
+        return render(request, 'pokemon.json', {'pokemon': pokemon}, content_type='application/json')
